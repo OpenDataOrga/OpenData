d655a96ecc0a30d270b4988996b66e0ddda2efcc
Michel--Picque Théodore <theodore.michelpicque@gmail.com>
2022-11-30 18:23:15 +0100

    update code

--- test.js
+++ test.js
@@ -3,57 +3,173 @@ var fs = require('fs')
 
 
 
-function searchOneBio(nomVille,file){
-    let openFile = fs.readFileSync(file)
-    let bioScore = JSON.parse(openFile)
+// function searchOneBio(nomVille,file){
+//     let openFile = fs.readFileSync(file)
+//     let bioScore = JSON.parse(openFile)
 
-    let res = []
-    bioScore.forEach(x => {
-        if (x.libellecommune == nomVille){
-            res.push(x.scoreBio)
-        }
-    })
+//     let res = []
+//     bioScore.forEach(x => {
+//         if (x.libellecommune == nomVille){
+//             res.push(x.scoreBio)
+//         }
+//     })
 
-    return res.reduce((partialSum, a) => partialSum + a, 0)
+//     return res.reduce((partialSum, a) => partialSum + a, 0)
 
-}
+// }
 
-console.log(searchOneBio('ARANDAS','Data/bioScore.json'))
-
-
-function mergeJson(){
-    let openFranceJson = fs.readFileSync('Data/france.json')
-    let franceJson = JSON.parse(openFranceJson)
-
-    let res = []
-    franceJson.forEach(x=> {
-        let tmp = new Object()
-        tmp.codeCommune = x.Code_commune_INSEE
-        tmp.nomCommune  = x.Nom_commune
-        res.push(tmp)
-    })
-
-    return res
-
-}
+// // console.log(searchOneBio('ARANDAS','Data/bioScore.json'))
 
 
-var dataTest = mergeJson()
+// function mergeJson(){
+//     let openFranceJson = fs.readFileSync('Data/france.json')
+//     let franceJson = JSON.parse(openFranceJson)
+
+//     let res = []
+//     franceJson.forEach(x=> {
+//         let tmp = new Object()
+//         tmp.codeCommune = x.Code_commune_INSEE
+//         tmp.nomCommune  = x.Nom_commune
+//         res.push(tmp)
+//     })
 
 
-console.log(dataTest)
+//     let openInternetJson = fs.readFileSync('Data/internet.json')
+//     let internetJson = JSON.parse(openInternetJson)
 
-console.log(typeof(dataTest))
+//     console.log(internetJson)
 
-console.log(typeof(dataTest[0]))
+//     internetJson.forEach(x=>{
+//         tmp = x.Code_INSEE
+//         res.forEach(y=>{
+//             if (tmp==y.codeCommune){
+//                 y['scoreInternet'] = x.Score_Internet
+//             }
+
+//         })
+//     })
 
 
-var data = JSON.stringify(dataTest)
+//     let openBBJson = fs.readFileSync('Resultats.json')
+//     let bBJson = JSON.parse(openBBJson)
+
+//     console.log(bBJson)
+
+//     bBJson.forEach(x=>{
+//         tmp = x.commune
+//         res.forEach(y=>{
+//             if(tmp==y.codeCommune){
+//                 console.log('toto')
+//                 y['scoreBB'] = x.Score
+//                 console.log(y['scoreBB'])
+//             }
+//         })
+//     })
+
+//     console.log(res)
 
 
-fs.writeFile("Data/test.json", data, function(err) {
-    if (err) {
-        console.log(err)
-    }
-})
+//     let openBioJson = fs.readFileSync('Data/bioScore.json')
+//     let bioJson = JSON.parse(openBioJson)
 
+//     bioJson.forEach(x=>{
+//         tmp = x.codecommune
+//         res.forEach(y=>{
+//             if(tmp==y.codeCommune){
+//                 y['scoreBio'] = x.scoreBio
+//             }
+//         })
+//     })
+
+
+//     let openGazJson = fs.readFileSync('Scripts/gaz_elec_final.json')
+//     let gazJson = JSON.parse(openGazJson)
+
+//     gazJson.forEach(x=>{
+//         tmp = x.commune
+//         res.forEach(y=>{
+//             if(tmp==y.codeCommune){
+//                 y['scoreGaz'] = x.indice_gaz
+//             }
+//         })
+//     })
+
+
+
+
+
+//     var dataTest = mergeJson()
+
+//     var data = JSON.stringify(dataTest)
+
+
+//     fs.writeFile("Data/test.json", data, function(err) {
+//         if (err) {
+//             console.log(err)
+//         }
+//     })
+
+// }
+
+
+// mergeJson()
+
+// function createdB(){
+//     let openFranceJson = fs.readFileSync('Data/france.json')
+//     let franceJson = JSON.parse(openFranceJson)
+
+//     res = new Object()
+//     //console.log(res)
+//     franceJson.forEach(x=>{
+//             //console.log(x.Nom_commune)
+//             res[x.Nom_commune] = []
+//             //console.log(res)
+//         }
+//     )
+
+//     //console.log(res)
+
+//     resStr = JSON.stringify(res)
+
+//     console.log(resStr)
+    
+//     fs.writeFile("Data/dbCom.json", resStr, function(err) {
+//         if (err) {
+//             console.log(err)
+//         }
+//     })
+
+// }
+
+// console.log('Start')
+
+// createdB()
+
+// function saveCom(content,target){
+//     let openFile = fs.readFileSync('Data/dbCom.json')
+//     let dbCom = JSON.parse(openFile)
+
+//     dbCom[target].push(content)
+
+//     dbComStr = JSON.stringify(dbCom)
+    
+//     fs.writeFile("Data/dbCom.json", dbComStr, function(err) {
+//         if (err) {
+//             console.log(err)
+//         }
+//     })
+
+// }
+
+// saveCom('test','CUCUGNAN')
+
+
+// function retrieveCom(target){
+//     let openFile = fs.readFileSync('Data/dbCom.json')
+//     let dbCom = JSON.parse(openFile)
+
+//     return dbCom[target]
+
+// }
+
+// console.log(retrieveCom('CUCUGNAN'))
